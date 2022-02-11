cf83dfd889cb26e5c70143f39efdbc8387d849e6
paolahydra <patella.paola@gmail.com>
2022-02-11 07:38:16 +0100

    fixed generalProbeRegistrationPipeline
    probes with all the info saved
    single script to use
    improved auto saving of script

--- paolas_pipeline/generalProbeRegistrationPipeline.m
+++ paolas_pipeline/generalProbeRegistrationPipeline.m
@@ -24,6 +24,9 @@ addpath(genpath('/Users/galileo/GitHub/allenCCF'))
 addpath(genpath('/Users/galileo/Documents/MATLAB/codeArberLab/anatomyRegistration/cortexLabCode/npy-matlab-master/npy-matlab'))
 pathToAtlas = '/Users/galileo/Documents/MATLAB/codeArberLab/anatomyRegistration/cortexLabCode/allen brain template files';
 
+genPipScriptLibrary = '/Users/galileo/GitHub/generalPipelineUsedScripts';
+repositoryTag = 'SR_GPR'; %SliceRegistration, SliceRegistration_ProbeRegistration, or SliceRegistrationConfocal pipeline - these scripts are added to gitignore, useful to know where they came from
+
 % addpath(genpath('C:\GitHub\allenCCF')) %clone the repository from : https://github.com/paolahydra/allenCCF/tree/sliceRegistration_confocal     
 % addpath(genpath('\\tungsten-nas.fmi.ch\tungsten\scratch\garber\BrainRegistration\code and atlas'))
 % pathToAtlas = '\\tungsten-nas.fmi.ch\tungsten\scratch\garber\BrainRegistration\code and atlas\allen brain template files\';
@@ -42,8 +45,8 @@ transformationType = 'pwl';     %use 'projective', or 'pwl' (piece-wise linear: 
 
 %%  set once, then always run: specify paths and settings for the specific brain to register
 % move your images to a local disk (SSD possibly) for much faster processing!
-image_folder = '/Users/galileo/dati/registered_brains_completed/ephys_probeLoc/1031709';   %change this
-image_tag = 'mouse_1031709_';                                               %change this - use an unequivocal tag for your experiment
+image_folder = '/Users/galileo/dati/registered_brains_completed/ephys_probeLoc/1031707';   %change this
+image_tag = 'mouse_1031707';                                               %change this - use an unequivocal tag for your experiment
 microns_per_pixel = 5.1803; %3.4536; %3.8852; %take this value from your tiff filename
 
 % increase gain if for some reason the images are not bright enough
@@ -55,19 +58,20 @@ end
 
 cd(image_folder)
 save_folder = fullfile(image_folder, 'startingSingleSlices');
-
+folder_processed_images = fullfile(save_folder, 'processed');
 
 %% do once, then skip: save the script and then create a new version with specific parameters - continue with the new script.
+A = matlab.desktop.editor.getActive;
+A.save;
+scriptname = fullfile(genPipScriptLibrary, sprintf('generalPipeline_%s_%s.m',repositoryTag, image_tag(1:end-1)));
 
-%save the script (generalPipeline.m)!!
-
-repositoryTag = 'SR_GPR'; %this is the SliceRegistrationConfocal pipeline - these scripts are added to gitignore, useful to know where they came from
-originalscript = which('generalPipeline');
-[a, b] = fileparts(originalscript);
-scriptname = fullfile(a, sprintf('generalPipeline_%s_%s.m',repositoryTag, image_tag(1:end-1)));
-copyfile(originalscript, scriptname)
-edit(scriptname)
-
+if strcmp(A.Filename, scriptname)
+    fprintf('Script %s already existing and in use.\n',sprintf('generalPipeline_%s_%s.m',repositoryTag, image_tag(1:end-1)))
+else
+    copyfile(A.Filename, scriptname)
+    A.close
+    matlab.desktop.editor.openAndGoToLine(scriptname, 75);
+end
 
 
 %% 1. do once, then skip: PP's preprocessing of axioscan images in ImageJ
@@ -175,10 +179,273 @@ T = saveTransformTable(fullfile(folder_processed_images, 'transformations'), ima
 % to the clicked point (see below). Thus you can verify the quality of the
 % fit. Below, the highlighted areas (in the reticular nucleus (top) and the
 % mediodorsal nucleus (bottom) of the thalamus) were clicked.p
-%%
-edit Display_Probe_Track.m
-% edit and run!
 
+%%
+
+%%
+
+%% edit Display_Probe_Track.m script has been copied down below so that mouse-specific info is saved too
+% edit and run!
+% ------------------------------------------------------------------------
+%          Display Probe Track
+% ------------------------------------------------------------------------
+
+%% ENTER PARAMETERS AND FILE LOCATION
+
+% % file location of probe points
+% folder_processed_images = 'C:\Drive\Histology\brainX\processed';
+% 
+% % directory of reference atlas files
+% annotation_volume_location = 'C:\Drive\Histology\for tutorial\annotation_volume_10um_by_index.npy';
+% structure_tree_location = 'C:\Drive\Histology\for tutorial\structure_tree_safe_2017.csv';
+
+% name of the saved probe points
+% probe_save_name_suffix = 'electrode_track_1';
+probe_save_name_suffix = '';
+
+% either set to 'all' or a list of indices from the clicked probes in this file, e.g. [2,3]
+probes_to_analyze = 'all';  % [1 2]
+
+% --------------
+% key parameters
+% --------------
+% how far into the brain did you go from the surface, either for each probe or just one number for all -- in mm
+probe_lengths = 4.500; 
+
+% from the bottom tip, how much of the probe contained recording sites -- in mm
+active_probe_length = 0.200;
+
+% number of sessions you turned down by an active length
+nSessions_down = 5;
+
+% distance queried for confidence metric -- in um
+probe_radius = 70; 
+
+% overlay the distance between parent regions in gray (this takes a while)
+show_parent_category = false; 
+
+% plot this far or to the bottom of the brain, whichever is shorter -- in mm
+distance_past_tip_to_plot = 0.5;
+
+% set scaling e.g. based on lining up the ephys with the atlas
+% set to *false* to get scaling automatically from the clicked points
+scaling_factor = false;    % 1.05-1.12 reasonable range
+
+
+% ---------------------
+% additional parameters
+% ---------------------
+% plane used to view when points were clicked ('coronal' -- most common, 'sagittal', 'transverse')
+plane = 'coronal';
+
+% probe insertion direction 'down' (i.e. from the dorsal surface, downward -- most common!) 
+% or 'up' (from a ventral surface, upward)
+probe_insertion_direction = 'down';
+
+% show a table of regions that the probe goes through, in the console
+show_region_table = true;
+      
+% black brain?
+black_brain = true;
+
+
+% close all
+
+
+
+
+%% GET AND PLOT PROBE VECTOR IN ATLAS SPACE
+
+% load the reference brain annotations
+if ~exist('av','var') || ~exist('st','var')
+    disp('loading reference atlas...')
+    av = readNPY(annotation_volume_location);
+    st = loadStructureTree(structure_tree_location);
+end
+
+% select the plane for the viewer
+if strcmp(plane,'coronal')
+    av_plot = av;
+elseif strcmp(plane,'sagittal')
+    av_plot = permute(av,[3 2 1]);
+elseif strcmp(plane,'transverse')
+    av_plot = permute(av,[2 3 1]);
+end
+
+% load probe points
+probePoints = load(fullfile(folder_processed_images, ['probe_points' probe_save_name_suffix]));
+ProbeColors = .75*[1.3 1.3 1.3; 1 .75 0;  .3 1 1; .4 .6 .2; 1 .35 .65; .7 .7 .9; .65 .4 .25; .7 .95 .3; .7 0 0; .6 0 .7; 1 .6 0]; 
+% order of colors: {'white','gold','turquoise','fern','bubble gum','overcast sky','rawhide', 'green apple','purple','orange','red'};
+fwireframe = [];
+
+% scale active_probe_length appropriately
+active_probe_length = active_probe_length*100;
+
+% determine which probes to analyze
+if strcmp(probes_to_analyze,'all')
+    probes = 1:size(probePoints.pointList.pointList,1);
+else
+    probes = probes_to_analyze;
+end 
+
+
+
+
+
+%% PLOT EACH PROBE -- FIRST FIND ITS TRAJECTORY IN REFERENCE SPACE
+
+% create a new figure with wireframe
+fwireframe = plotBrainGrid([], [], fwireframe, black_brain);
+hold on; 
+fwireframe.InvertHardcopy = 'off';
+
+for selected_probe = probes
+    
+% get the probe points for the currently analyzed probe 
+if strcmp(plane,'coronal')
+    curr_probePoints = probePoints.pointList.pointList{selected_probe,1}(:, [3 2 1]);
+elseif strcmp(plane,'sagittal')
+    curr_probePoints = probePoints.pointList.pointList{selected_probe,1}(:, [1 2 3]);
+elseif strcmp(plane,'transverse')
+    curr_probePoints = probePoints.pointList.pointList{selected_probe,1}(:, [1 3 2]);
+end
+
+
+
+% get user-defined probe length from experiment
+if length(probe_lengths) > 1
+    probe_length = probe_lengths(selected_probe);
+else
+    probe_length = probe_lengths;
+end
+
+% get the scaling-factor method to use
+if scaling_factor
+    use_tip_to_get_reference_probe_length = false;
+    reference_probe_length = probe_length * scaling_factor;
+    disp(['probe scaling of ' num2str(scaling_factor) ' determined by user input']);    
+else
+    use_tip_to_get_reference_probe_length = true;
+    disp(['getting probe scaling from histology data...']);
+end
+
+% get line of best fit through points
+% m is the mean value of each dimension; p is the eigenvector for largest eigenvalue
+[m,p,s] = best_fit_line(curr_probePoints(:,1), curr_probePoints(:,2), curr_probePoints(:,3));
+if isnan(m(1))
+    disp(['no points found for probe ' num2str(selected_probe)])
+    continue
+end
+
+% ensure proper orientation: want 0 at the top of the brain and positive distance goes down into the brain
+if p(2)<0
+    p = -p;
+end
+
+% determine "origin" at top of brain -- step upwards along tract direction until tip of brain / past cortex
+ann = 10;
+out_of_brain = false;
+while ~(ann==1 && out_of_brain) % && distance_stepped > .5*active_probe_length)
+    m = m-p; % step 10um, backwards up the track
+    ann = av_plot(round(m(1)),round(m(2)),round(m(3))); %until hitting the top
+    if strcmp(st.safe_name(ann), 'root')
+        % make sure this isn't just a 'root' area within the brain
+        m_further_up = m - p*25; % is there more brain 250 microns up along the track?
+        ann_further_up = av_plot(round(max(1,m_further_up(1))),round(max(1,m_further_up(2))),round(max(1,m_further_up(3))));
+        if strcmp(st.safe_name(ann_further_up), 'root')
+            out_of_brain = true;
+        end
+    end
+end
+
+% focus on wireframe plot
+figure(fwireframe);
+
+% plot probe points
+hp = plot3(curr_probePoints(:,1), curr_probePoints(:,3), curr_probePoints(:,2), '.','linewidth',2, 'color',[ProbeColors(selected_probe,:) .2],'markers',10);
+
+% plot brain entry point
+plot3(m(1), m(3), m(2), 'r*','linewidth',1)
+
+% use the deepest clicked point as the tip of the probe, if no scaling provided (scaling_factor = false)
+if use_tip_to_get_reference_probe_length
+    % find length of probe in reference atlas space
+    if strcmp(probe_insertion_direction, 'down')
+        [depth, tip_index] = max(curr_probePoints(:,2));
+    elseif strcmp(probe_insertion_direction, 'up')
+        [depth, tip_index] = min(curr_probePoints(:,2));    
+    end
+    reference_probe_length_tip = sqrt(sum((curr_probePoints(tip_index,:) - m).^2)); 
+    
+    % and the corresponding scaling factor
+    shrinkage_factor = (reference_probe_length_tip / 100) / probe_length;
+    
+    % display the scaling
+    disp(['probe length of ' num2str(reference_probe_length_tip/100) ' mm in reference atlas space compared to a reported ' num2str(probe_length) ' mm']);
+    disp(['probe scaling of ' num2str(shrinkage_factor)]); disp(' ');
+    
+    % plot line the length of the probe in reference space
+    probe_length_histo = round(reference_probe_length_tip);
+    
+% if scaling_factor is user-defined as some number, use it to plot the length of the probe
+else 
+    probe_length_histo = round(reference_probe_length * 100); 
+end
+
+% find the percent of the probe occupied by electrodes
+percent_of_tract_with_active_sites = min([active_probe_length / (probe_length*100), 1.0]);
+active_site_start = probe_length_histo*(1-percent_of_tract_with_active_sites);
+active_probe_position = round([active_site_start  probe_length_histo]);
+
+% plot line the length of the active probe sites in reference space
+plot3(m(1)+p(1)*[active_probe_position(1) active_probe_position(2)], m(3)+p(3)*[active_probe_position(1) active_probe_position(2)], m(2)+p(2)*[active_probe_position(1) active_probe_position(2)], ...
+    'Color', ProbeColors(selected_probe,:), 'LineWidth', 1);
+% plot line the length of the entire probe in reference space
+plot3(m(1)+p(1)*[1 probe_length_histo], m(3)+p(3)*[1 probe_length_histo], m(2)+p(2)*[1 probe_length_histo], ...
+    'Color', ProbeColors(selected_probe,:), 'LineWidth', 1, 'LineStyle',':');
+
+
+%% ----------------------------------------------------------------
+% Get and plot brain region labels along the extent of each probe
+% ----------------------------------------------------------------
+
+% convert error radius into mm
+error_length = round(probe_radius / 10);
+
+% find and regions the probe goes through, confidence in those regions, and plot them
+probeRecap(selected_probe).borders_table = plotDistToNearestToTip(m, p, av_plot, st, probe_length_histo, error_length, active_site_start, distance_past_tip_to_plot, show_parent_category, show_region_table, plane); % plots confidence score based on distance to nearest region along probe
+title(['Probe ' num2str(selected_probe)],'color',ProbeColors(selected_probe,:))
+for nsd = 1:nSessions_down
+    plot([0 100], [(active_site_start*10) (active_site_start*10)], 'color',[.1 .1 .1], 'LineStyle',':', 'linewidth',3);
+end
+savefig(fullfile(folder_processed_images, sprintf('bordersTable_probe%d.fig',selected_probe)))
+export_fig(fullfile(folder_processed_images, sprintf('bordersTable_%s_probe%d.png',image_tag, selected_probe)), '-m5');
+
+% add some more digested info to proberecap and save it
+probeRecap(selected_probe).shrinkage_factor = shrinkage_factor;
+probeRecap(selected_probe).active_probe_position = active_probe_position;
+% % there would be some rounding issue creating a mismatch between the
+% % figure and the info here, if using this method:
+% probeRecap(selected_probe).DVstart = 10* (probeRecap(selected_probe).active_probe_position(1) - diff(probeRecap(selected_probe).active_probe_position)* (0:nSessions_down-1) );
+% probeRecap(selected_probe).DVtip =   10* (probeRecap(selected_probe).active_probe_position(2) - diff(probeRecap(selected_probe).active_probe_position)* (0:nSessions_down-1) );
+% probeRecap(selected_probe).adjustedActivelength = 10*diff(probeRecap(selected_probe).active_probe_position);
+
+% this is the same method used in the figure, with no rounding to the next 10.
+actL = 10*(probe_length_histo-active_site_start);
+probeRecap(selected_probe).adjustedActivelength = actL;
+probeRecap(selected_probe).DVstart = round(10*active_site_start - actL*(0:nSessions_down-1));
+probeRecap(selected_probe).DVtip =   round(10*probe_length_histo - actL*(0:nSessions_down-1));
+
+Probe = probeRecap(selected_probe);
+save(fullfile(folder_processed_images, sprintf('Probe%d.mat',selected_probe)), '-struct', 'Probe');
+
+end
+
+
+
+%%
+
+%%
 
 %% load cells (retro_fromMed in github)
 folders2Include={'/Users/galileo/dati/registered_brains_completed/992234', ...
@@ -213,7 +480,7 @@ for i = 1:length(S)
     
 end
 %%
-makeVideo = 1;
+makeVideo = 0;
 if makeVideo   
     F = -29.2;
     view([F, 17])
@@ -236,13 +503,3 @@ end
 
 
 
-%%
-i = 1; figure(4+i)
-export_fig(sprintf('%s_probeTract_probe%02d.png',image_tag, i), '-m5'); i=i+1;
-%%
-figure(4+i)
-export_fig(sprintf('%s_probeTract_probe%02d.png',image_tag, i), '-m5'); i=i+1;
-figure(4+i)
-export_fig(sprintf('%s_probeTract_probe%02d.png',image_tag, i), '-m5'); i=i+1;
-figure(4+i)
-export_fig(sprintf('%s_probeTract_probe%02d.png',image_tag, i), '-m5'); i=i+1;
